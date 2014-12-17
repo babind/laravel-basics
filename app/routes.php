e59476332452fc6f3ef104eb340da4d980c0c9b0
bashantad <basantad2@gmail.com>
2014-12-17 21:35:31 +1100

    uncommented '/' on routes

--- app/routes.php
+++ app/routes.php
@@ -2,87 +2,12 @@
 
 
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-// Route::get('users','UsersController@index');
-// Route::get('users' ,'UsersController@create');
-
-// // Route::get('users',function()
-// // {
-// // 	$users=User::all();
-
-// // 	//return View::make('users/index')->with('users',$users);
-// // 	//or 
-// // 	//return View::make('users/index')->withUsers($users);
-// // 	//or
-// // 	return View::make('users/index',['users'=>$users]);
-// // });
-
-// Route::get('users/{username}',function($username)
-// {
-	 
-// });
-
-
-// Route::get('/', function() 
-
-// {
-// 	// $users=DB::table('user')->get();
-// 	// return $users;
-// 	// dd($users);// die(var_dump($user));
-// 	// $users=DB::table('users')->where('username','!=','jeffreyWay')->get();
-// 	// return $users;
-// 	// $users=user::where('username','!=','jeffreyway')->get();
-// 	// return $users;
-
-// 	// $user=new User;
-// 	// $user->username='NewUser';
-// 	// $user->password=Hash::make('password');
-// 	// $user->save();
-// 	// //$user=DB::insert("insert into user (username,password) VALUES(:username,:password)");
-// 	// return User::all();
-// 	// User::create([
-// 	// 	'username'=>'AnotherUser',
-// 	// 	'password'=>Hash::make('1234')
-// 	// 	]);
-// 	// return User::all();
-// 	//  to update the user using orm in laravel.
-// 	// 	$user=User::find(2);
-// 	// 	$user->username='UpdatedName';
-// 	// 	$user->save();
-	
-// 	// 	return User::all();
-
-// 	// To delete the user having the id of that 
-// 	// $user=User::find(1);
-// 	// $user->delete();
-// 	// return User::all();
-// 	// to get user by user by descending order
-// 	// return User::orderBy('username','des')->get();
-// 	// return User::orderBy('username','asc')->take(2)->get();
-// });
-
- // Route::get('/',function()
-
- // {
-
- // 	User::create([
- // 		'username'=>'Jeffreyway',
- // 		'password'=>Hash::make('changeme'),
- // 		'email'=>'jeffreyway@laracasts.com'
- // 		]);
- // 	Return 'Done';
- // });
+Route::get('/',function()
+{
+	return View::make("home");
+});
 Route::resource('sessions','SessionsController');
 
- Route::get('login','SessionsController@create');
- Route::get('logout','SessionsController@destroy');
+Route::get('login','SessionsController@create');
+Route::get('logout','SessionsController@destroy');
  	
---
+++ app/views/home.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel Basics</title>	
+</head>
+<body>
+	<div class="container">
+		<a href="{{URL::to("sessions/create")}}">Sign in</a>	
+	</div>
+</body>
+</html>
